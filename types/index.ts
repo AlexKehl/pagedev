f3d5eef10c868c2a0012ba3489ccb4ff6de67b4c
Alex Kehl <alexander.kehl@apprologic.de>
2021-11-15 13:03:57 +0100

    Add types

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,13 @@
 export type ValueOf<T> = T[keyof T]
 
 export type nil = undefined | null
+
+export type AsyncReturnType<T extends (...args: any) => any> = T extends (
+  ...args: any
+) => Promise<infer U>
+  ? U
+  : T extends (...args: any) => infer U
+  ? U
+  : any
+
+export type Fn = (...args: any[]) => any
